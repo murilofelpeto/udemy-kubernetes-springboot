85ea4bb5772b79a191f5db7d6e66a1b0c94ee813
murilofelpeto <murilo.felpeto@ifood.com.br>
2020-03-24 16:11:33 -0300

    Create Currency conversion service

---
+++ 05 - currency-conversion-service/src/test/java/br/com/murilo/currencyexchangemicroservice/CurrencyExchangeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.murilo.currencyexchangemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
