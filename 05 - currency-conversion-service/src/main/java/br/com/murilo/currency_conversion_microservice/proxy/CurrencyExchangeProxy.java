8b23ad3346b0d29f6ed61a5851c8b03937676bfd
murilofelpeto <murilo.felpeto@ifood.com.br>
2020-03-26 15:24:57 -0300

    Make ingress available and changed LoadBalancer to NodePorts

--- 05 - currency-conversion-service/src/main/java/br/com/murilo/currency_conversion_microservice/proxy/CurrencyExchangeProxy.java
+++ 05 - currency-conversion-service/src/main/java/br/com/murilo/currency_conversion_microservice/proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.murilo.currency_conversion_microservice.bean.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:${CURRENCY_EXCHANGE_SERVICE_PORT:8080}") //
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:${CURRENCY_EXCHANGE_PORT:8080}") //
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+//@FeignClient(name = "currency-exchange-service")//Kubernetes Service Name
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
